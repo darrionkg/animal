59ef52b663702d6c2871b54fbc7ae2df80b48b9b
darrion gering and jaime takikawa <darrion@>
2019-03-28 11:21:00 -0700

    Add initial files

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   $("#tiger").click(function() {
     $("#showTiger").show();
+    $("#showSloth").hide();
+    $("#showPython").hide();
+
   });
   $("#sloth").click(function() {
     $("#showSloth").show();
+    $("#showTiger").hide();
+    $("#showPython").hide();
   });
   $("#python").click(function() {
     $("#showPython").show();
+    $("#showSloth").hide();
+    $("#showTiger").hide();
   });
 });
